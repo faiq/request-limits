2898ae3197aa5204de50cd2c5e26e2e560abf16a
faiq <faiqrazarizvi@gmail.com>
2015-02-25 17:15:29 -0500

    adds tests

--- test/test.js
+++ test/test.js
@@ -1,18 +1,31 @@
 var request = require('request')
   , http = require('http')
   , crypto = require('crypto')
-  , limit = require('./index')
+  , limit = require('../index')
+  , Code = require('code') 
+  , Lab = require('lab')
+  , lab = exports.lab = Lab.script()
+  , describe = lab.describe
+  , it = lab.it
+  , before = lab.before
+  , expect = Code.expect
 
-http.createServer(function (req, res) {
-  crypto.randomBytes(256, function(ex, buf) {
-    if (ex) throw ex
-    res.end(buf)
+describe('a request thats too big', function () { 
+  it ('should throw an error', function (done) {  
+    http.createServer(function (req, res) {
+        crypto.randomBytes(256, function(ex, buf) {
+          if (ex) throw ex
+          res.end(buf)
+        })
+      }).listen(1337, '127.0.0.1')
+     
+    var opts = {uri:'http://127.0.0.1:1337'}
+      , s = request(opts, function (e, r, b) { 
+          expect(e).to.exist()
+          expect(r).to.not.exist()
+          expect(b).to.not.exist()
+          done()
+        })
+    limit(s, 255)
   })
-}).listen(1337, '127.0.0.1')
-
-var opts = {uri:'http://127.0.0.1:1337'}
-  , s = request(opts, function (e, r, b) { 
-      if (e) console.error(e)
-    });
-
-limit(s, 255) 
+})
